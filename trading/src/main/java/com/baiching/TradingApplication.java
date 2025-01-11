cd5ed8e3c42c1ff5438ba44b1045a962ee9170b2
baiching <bachmarma@gmail.com>
2025-01-11 13:38:32 +0100

    removed authentication at the begining of the application

--- trading/src/main/java/com/baiching/TradingApplication.java
+++ trading/src/main/java/com/baiching/TradingApplication.java
@@ -3,7 +3,7 @@ package com.baiching;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.baiching.*"})
 public class TradingApplication {
 
 	public static void main(String[] args) {
--- trading/src/main/java/com/baiching/controller/HomeController.java
+++ trading/src/main/java/com/baiching/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.baiching.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HomeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping
-    public String home() {
+    public @ResponseBody String home() {
+        //logger.info("Home endpoint called");
         return "welcome to trading platform";
+    }
+
+    @GetMapping("/api")
+    public String secure() {
+        return "welcome to trading platform secure";
     }
 }
